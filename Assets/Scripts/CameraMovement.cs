5521b9edd0bc9394938b1bc35833eb8d66c91933
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-08-16 16:16:28 -0300

    CameraMovement added

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Vector2 distance;
+    public Player player;
+    void Start()
+    {
+        player.OnFollowingPlayer += FollowPlayer;
+    }
+    private void OnDestroy()
+    {
+        player.OnFollowingPlayer -= FollowPlayer;
+    }
+    void FollowPlayer()
+    {
+        Vector3 pos;
+        pos = player.transform.position;
+        pos.z += distance.x;
+        pos.y -= distance.y;
+        transform.position = pos;
+    }
+}
