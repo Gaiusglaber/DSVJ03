67978708a41c5cb87a3f8bf648664be41f3e25eb
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-08-21 16:31:06 -0300

    Toon shader added to proyect

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float distance = 5;
+    [SerializeField] public GameObject UI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, distance))
+        {
+            if (hit.transform.CompareTag("Door"))
+            {
+                UI.SetActive(true);
+            }
+        }
+        else
+        {
+            UI.SetActive(false);
+        }
+    }
+}
