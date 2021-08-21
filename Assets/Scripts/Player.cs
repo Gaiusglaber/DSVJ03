58d6c6eb553af761d969bf8961a3ea820fc269b7
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-08-21 16:48:56 -0300

    2nd jump mechanic added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] Rigidbody rigidBody;
+    [SerializeField] float jumpSpeed = 12f;
     [SerializeField] private float distance = 5;
     [SerializeField] public GameObject UI;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -26,6 +28,18 @@ public class Player : MonoBehaviour
         else
         {
             UI.SetActive(false);
+        }
+        JumpMechanic();
+    }     
+    void JumpMechanic()
+    {
+        float YVelocity = rigidBody.worldCenterOfMass.y;
+        if (YVelocity >0.9f&& YVelocity!=0)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                rigidBody.AddForce(new Vector3(0, jumpSpeed*50, 0));
+            }
         }
     }
 }
