d9e7c5f81daaf9472a2557d6abe1e616ac7d50a8
Juan <juanrodriguezgiles@gmail.com>
2021-08-31 20:07:25 -0300

    Implemented basic slope sliding

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed;
     [SerializeField] private float _walkSpeed;
     [SerializeField] private float _runSpeed;
+    [SerializeField] private float _slideSpeed;
     [SerializeField] private bool _isGrounded;
     [SerializeField] private float _groundCheckDistance;
     [SerializeField] private LayerMask _groundMask;
     [SerializeField] private float _gravity;
     [SerializeField] private float _jumpHeight;
+    private float _groundRayDistance = 1;
+    private RaycastHit _slopeHit;
 
     private Vector3 _moveDirection;
     private Vector3 _velocity;
@@ -18,13 +22,16 @@ public class PlayerMovement : MonoBehaviour
     {
         _controller = GetComponent<CharacterController>();
     }
+
     void Update()
     {
         Move();
+        if (OnSteepSlope()) SteepSlopeMovement();
     }
+
     private void Move()
     {
-        _isGrounded = Physics.CheckSphere(transform.position, _groundCheckDistance, _groundMask);
+        _isGrounded = Physics.CheckSphere(transform.position, _groundCheckDistance,_groundMask);
 
         if (_isGrounded && _velocity.y < 0)
         {
@@ -49,31 +56,61 @@ public class PlayerMovement : MonoBehaviour
             {
                 Idle();
             }
+
             _moveDirection *= _moveSpeed;
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 Jump();
             }
         }
+
         _controller.Move(_moveDirection * Time.deltaTime);
 
         _velocity.y += _gravity * Time.deltaTime;
         _controller.Move(_velocity * Time.deltaTime);
     }
+
     private void Idle()
     {
 
     }
+
     private void Walk()
     {
         _moveSpeed = _walkSpeed;
     }
+
     private void Run()
     {
         _moveSpeed = _runSpeed;
     }
+
     private void Jump()
     {
         _velocity.y = Mathf.Sqrt(_jumpHeight * -2 * _gravity);
+    }
+
+    private bool OnSteepSlope()
+    {
+        if (!_isGrounded) return false;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out _slopeHit,
+            (_controller.height / 2) + _groundRayDistance))
+        {
+            float _slopeAngle = Vector3.Angle(_slopeHit.normal, Vector3.up);
+            if (_slopeAngle > _controller.slopeLimit) return true;
+        }
+
+        return false;
+    }
+    private void SteepSlopeMovement()
+    {
+        Vector3 slopedirection = Vector3.up - _slopeHit.normal * Vector3.Dot(Vector3.up, _slopeHit.normal);
+        float slideSpeed = _moveSpeed + _slideSpeed;
+
+        _moveDirection = slopedirection * -slideSpeed;
+        _moveDirection.y = _moveDirection.y - _slopeHit.point.y;
+
+        _controller.Move(_moveDirection * Time.deltaTime);
     }
 }
