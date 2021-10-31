65c50475daa3eb587ca9b2513c2887219ece1498
JuanRodriguezGiles <juanrodriguezgiles@gmail.com>
2021-10-31 20:09:05 -0300

    Added Editor script for collectables to avoid clipping

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -27,18 +27,18 @@ public class CameraFollow : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            dragOrigin = Input.mousePosition;
-            return;
-        }
+        //if (Input.GetMouseButtonDown(0))
+        //{
+        //    dragOrigin = Input.mousePosition;
+        //    return;
+        //}
 
-        if (!Input.GetMouseButton(0)) return;
+        //if (!Input.GetMouseButton(0)) return;
 
-        Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
-        Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
+        //Vector3 pos = Camera.main.ScreenToViewportPoint(Input.mousePosition - dragOrigin);
+        //Vector3 move = new Vector3(pos.x * dragSpeed, 0, pos.y * dragSpeed);
 
-        transform.Translate(move, Space.World);
+        //transform.Translate(move, Space.World);
     }
     void LateUpdate()
     {
---
+++ Assets/Scripts/CollectableHover.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+[ExecuteAlways]
+public class CollectableHover : MonoBehaviour
+{
+    [SerializeField] private LayerMask _groundMask;
+
+    void Update()
+    {
+        if (Physics.CheckSphere(transform.position, 1, _groundMask))
+        {
+            transform.position += new Vector3(0, 1, 0);
+        }
+    }
+}
