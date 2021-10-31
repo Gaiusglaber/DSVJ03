82363994ef633ca4a2a6777552e712b3768e1f80
JuanRodriguezGiles <juanrodriguezgiles@gmail.com>
2021-10-31 20:58:05 -0300

    Implemented colliders to change slope limit on ramp

---
+++ Assets/Scripts/SlopeChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SlopeChanger : MonoBehaviour
+{
+    [SerializeField] private bool _up;
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.tag != "Player") return;
+        CharacterController controller = col.GetComponent<CharacterController>();
+        if (_up)
+            controller.slopeLimit = 20;
+        else if (controller.slopeLimit == 20)
+            controller.slopeLimit = 35;
+        else
+            controller.slopeLimit = 20;
+    }
+    void OnTriggerExit(Collider col)
+    {
+        if (col.tag != "Player") return;
+        if (!_up)
+        {
+            CharacterController controller = col.GetComponent<CharacterController>();
+            controller.slopeLimit = 35;
+        }
+    }
+}
