071a8ba96715ab0c9910374b6cd9c67b26a21be8
JuanRodriguezGiles <juanrodriguezgiles@gmail.com>
2021-10-31 19:17:18 -0300

    Implemented spin animation

---
+++ Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+public class PlayerAttack : MonoBehaviour
+{
+    private Animator _animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            _animator.SetBool("Attacking",true);
+        }
+
+        if (_animator.GetBool("Attacking") && _animator.GetCurrentAnimatorStateInfo(0).IsName("SpinEnd"))
+        {
+            _animator.SetBool("Attacking", false);
+        }
+    }
+}
