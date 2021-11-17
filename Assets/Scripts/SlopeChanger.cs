9691bb094dcd83dbbca0ebdf3f29d7e360f6e167
JuanRodriguezGiles <juanrodriguezgiles@gmail.com>
2021-11-17 01:14:45 -0300

    Changed ramp detection logic

--- Assets/Scripts/SlopeChanger.cs
+++ Assets/Scripts/SlopeChanger.cs
@@ -3,25 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 public class SlopeChanger : MonoBehaviour
 {
-    [SerializeField] private bool _up;
+    private float aux;
     void OnTriggerEnter(Collider col)
     {
         if (col.tag != "Player") return;
         CharacterController controller = col.GetComponent<CharacterController>();
-        if (_up)
-            controller.slopeLimit = 20;
-        else if (controller.slopeLimit == 20)
-            controller.slopeLimit = 35;
-        else
-            controller.slopeLimit = 20;
+        aux = controller.slopeLimit;
+        controller.slopeLimit = 0;
     }
     void OnTriggerExit(Collider col)
     {
         if (col.tag != "Player") return;
-        if (!_up)
-        {
-            CharacterController controller = col.GetComponent<CharacterController>();
-            controller.slopeLimit = 35;
-        }
+        CharacterController controller = col.GetComponent<CharacterController>();
+        controller.slopeLimit = aux;
     }
 }
