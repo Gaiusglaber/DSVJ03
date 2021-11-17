4c55c4b1d2eb010a75a7f90312c145dad09ead36
JuanRodriguezGiles <juanrodriguezgiles@gmail.com>
2021-11-17 01:15:40 -0300

    Removed unused method

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
+﻿using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] Rigidbody rigidBody;
@@ -23,30 +20,6 @@ public class Player : MonoBehaviour
             if (hit.transform.CompareTag("Door"))
             {
             }
-        }
-        else
-        {
-        }
-        JumpMechanic();
-    }
-    void JumpMechanic()
-    {
-        float YVelocity = rigidBody.velocity.y;
-        if (YVelocity > 0)
-        {
-            if (Input.GetKeyDown(KeyCode.Space) && bounceCounter < 1)
-            {
-                rigidBody.AddForce(new Vector3(0, jumpSpeed * 50, 0));
-                bounceCounter++;
-            }
-            else if (Input.GetKeyDown(KeyCode.W))
-            {
-                rigidBody.AddForce(transform.forward * jumpSpeed * 25);
-            }
-        }
-        else if (YVelocity == 0)
-        {
-            bounceCounter = 0;
         }
     }
 }
