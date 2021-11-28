b07001cceecb98c47310a97be6bf88f0903011fd
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-11-28 13:10:37 -0300

    Hotfixed plataform.

--- Assets/Scripts/Plataform.cs
+++ Assets/Scripts/Plataform.cs
@@ -173,8 +173,8 @@ public class Plataform : MonoBehaviour
     }
     IEnumerator PlataformStartBreak(Vector3 initialPos)
     {
-        GetComponent<Animator>().SetTrigger("Break");
         yield return new WaitForSeconds(timeToBreak);
+        GetComponent<Animator>().SetTrigger("Break");
         Destroy(gameObject);
     }
 }
